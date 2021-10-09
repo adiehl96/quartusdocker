ba346c4ea45c89e765c20b612f821b1426ff4afd
adiehl96 <a.diehl@student.ru.nl>
2021-10-09 21:26:28 +0200

    first working dockerfile

--- debian/run.sh
+++ debian/run.sh
@@ -1,4 +1,4 @@
-docker build -t quartus --platform linux/amd64 .
-docker run -e DISPLAY=192.168.0.103:0 --name quartus -d quartus
 wget -N https://archive.eclipse.org/technology/epp/downloads/release/mars/2/eclipse-cpp-mars-2-linux-gtk-x86_64.tar.gz
 wget -N https://download.altera.com/akdlm/software/acdsinst/20.1std.1/720/ib_tar/Quartus-lite-20.1.1.720-linux.tar
+docker build -t quartus .
+docker run -e DISPLAY=192.168.0.104:0 --name quartus -d quartus --mac-address="12:34:de:b0:6b:61"
